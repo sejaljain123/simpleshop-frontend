9a9dd202e8d4c96e5714ea3ec1925d85491ef043
poorvi-vaish <poorvivaish11010@gmail.com>
2022-12-22 14:34:35 +0530

    Complete Invoice Form

--- src/components/Share Invoice/ShareInvoice.tsx
+++ src/components/Share Invoice/ShareInvoice.tsx
@@ -1,5 +1,6 @@
-import {FiSend} from "react-icons/fi";
-import Input from '../Input/Input';
+import { FiSend } from "react-icons/fi";
+import Input from "../Input/Input";
+import PrivewModal from '../PreviewModal/PreviewModal';
 
 const ShareInvoice = () => {
   return (
@@ -8,24 +9,45 @@ const ShareInvoice = () => {
         <FiSend />
         Share Invoice
       </button>
-      <button className="btn m-1 m-1">
-        Download
-      </button>
+      <div className="flex justify-around">
+      <button className="btn m-1 w-1/2">Download</button>
+      <PrivewModal />
+      </div>
       <hr />
-      <span className="text-md mt-4 p-3">Currency  <Input label="INR (Indian National Rupee)" name="currency" type="text" styles="m-1"/>
+      <span className="text-md mt-4 p-3 font-medium">
+        Currency{" "}
+        <Input
+          label="INR (Indian National Rupee)"
+          name="currency"
+          type="text"
+          styles="m-1"
+          {...{ disabled: true }}
+        />
       </span>
       <div className="flex flex-col p-3">
-        <span>Payment Method</span>
+        <span className="text-md mt-4 p-3 font-medium">Payment Method</span>
         <label>
-          <input type="radio" name="radio-1" className="radio-xs radio-primary" />
+          <input
+            type="radio"
+            name="radio-1"
+            className="radio-xs radio-primary"
+          />
           <span className="text-md px-2">Cash</span>
         </label>
         <label>
-          <input type="radio" name="radio-1" className="radio-xs radio-primary" />
+          <input
+            type="radio"
+            name="radio-1"
+            className="radio-xs radio-primary"
+          />
           <span className="text-md px-2">Cheque</span>
         </label>
         <label>
-          <input type="radio" name="radio-1" className="radio-xs radio-primary" />
+          <input
+            type="radio"
+            name="radio-1"
+            className="radio-xs radio-primary"
+          />
           <span className="text-md px-2">UPI</span>
         </label>
       </div>
